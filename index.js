3075f8349afd37f91524531c9157dfac99c08377
santosh <santoshacharya2468@gmail.com>
2021-05-13 14:21:37 +0545

    'nice'

---
+++ index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+app.use('/',express.static('public'));
+app.get('/',(req,res)=>{
+    res.sendFile('./public/index.html');
+});
+app.listen(process.env.PORT||3000,()=>console.log('Server running'));
